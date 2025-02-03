0d734ae3075befcb950c6ebfc9d7ba07ce13339d
shinjiyong <shinjiyong0904@gmail.com>
2025-02-03 12:43:36 +0900

    피라미드

--- Program.cs
+++ Program.cs
@@ -5,17 +5,16 @@ namespace L20250203
     internal class Program
     {
         static void Main(string[] args)
-        {
-            //*****
-            for(int i=0; i<5; i++)
+        {            
+            for (int j = 0; j < 5; j++)
             {
-                for (int j = 0; j < 5; j++) 
+                for (int i = 0; i < (j+1); i++)
                 {
                     Console.Write('*');
                 }
                 Console.WriteLine();
             }
-
+            
         }
     }
 }
